2175b516b9beef5983dcea5333cf888acb3231d6
Xenosgrilda <dualmeidalima@gmail.com>
2019-09-20 21:25:40 -0300

    Employee Controller Edit and Save

--- src/main/java/com/xenosgrilda/datatablesthymeleafspringboot/controller/EmployeeController.java
+++ src/main/java/com/xenosgrilda/datatablesthymeleafspringboot/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.xenosgrilda.datatablesthymeleafspringboot.controller;
 import com.xenosgrilda.datatablesthymeleafspringboot.entity.Employee;
 import com.xenosgrilda.datatablesthymeleafspringboot.service.EmployeeService;
 import lombok.AllArgsConstructor;
+
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -37,7 +39,8 @@ public class EmployeeController {
     @GetMapping("/edit-form/{id}")
     public ModelAndView editFormEmployee(@PathVariable int id, Employee emp){
 
-        if (id == -1){
+        if ( id == -1){
+            emp.setId(id);
             return new ModelAndView("employee/edit").addObject("employee",emp);
         } else {
             emp = this.employeeService.findById(id);
@@ -48,14 +51,19 @@ public class EmployeeController {
         }
     }
 
+    @GetMapping("/create")
+    public ModelAndView createEmployee() {
+        Employee emp = new Employee();
+        return this.editFormEmployee(-1, emp);
+    }
 
-//    @GetMapping("/delete")
-//    public String deleteEmployee(@RequestParam(value = "id") int id) {
-//
-//        this.employeeService.deleteById(id);
-//
-//        return "redirect:/employees/home";
-//    }
+    @GetMapping("/delete")
+    public String deleteEmployee(@RequestParam(value = "id") int id) {
+
+        this.employeeService.deleteById(id);
+
+        return "redirect:/employees/home";
+    }
 
     @PostMapping("/edit")
     public ModelAndView editEmployee(@Valid Employee employee, BindingResult bindingResult) {
@@ -71,7 +79,7 @@ public class EmployeeController {
             // Faking home
             List<Employee> employeeList = this.employeeService.findAllByOrderByLastNameAsc();
 
-            return new ModelAndView("employee/home").addObject("employees", employeeList);
+            return new ModelAndView("redirect:/employees/home").addObject("employees", employeeList);
         }
     }
 }
@@ -79,3 +87,8 @@ public class EmployeeController {
 /**
  * On "editEmployee" @ModelAttribute receives the name of the Model being sent from the view
  */
+
+ /**
+  * Remember that the name of the object "nameOfTheObject" added in "ModelAndView("...").addObject("nameOfTheObject", object)" needs to
+  * be the same as object's class, otherwise BindingResult will get lost.
+  */
